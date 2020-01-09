109a133ff574326e486cd71bfaba6da48b542b2b
sphuyal <sphuyal0@gmail.com>
2020-01-09 18:28:52 +0545

    Login and Register Activity layout

---
+++ app/src/main/java/com/sandesh/hamrobazar/LoginActivity.java
@@ -0,0 +1,17 @@
+package com.sandesh.hamrobazar;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Dialog;
+import android.os.Bundle;
+import android.widget.EditText;
+
+public class LoginActivity extends AppCompatActivity {
+    private EditText etEmail, etPassword;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
